871740290613efdb57075614fcf8b48ee122d00d
JasonBoyett <jason.boyett@gmail.com>
2022-10-07 09:44:24 -0500

    fixed spelling and removed unused getter

--- make_db.py
+++ make_db.py
@@ -12,11 +12,11 @@ table_name = "games"#name of the table you wish to create
 if __name__ == '__main__':#ignore this it's here for testing reasons
 
     db_schema = ("name text", "developer text", "release_year integer") #the naming convention for your table names is that all words are lower case and that words are seperated by an underscore.
-    #the second word in each of the peices of yoru scema touple is the data type heald by that feild
+    #the second word in each of the pieces of your schema topple is the data type held by that field
 
 
-    table = [#this is a tuple, tooples are immutable and can be passed as an argument in sqlite to create or edit a table
-        #I'm using an example of video games for thie databese but you will edit this toople to fit your particular databse
+    table = [#this is a tuple, topples are immutable and can be passed as an argument in sqlite to create or edit a table
+        #I'm using an example of video games for this database but you will edit this topple to fit your particular database
         ("Minecraft", "Notch", 2011 ),
         ("Sekiro Shadows Die Twice", "From Software", 2019),
         ("Call of Duty 4", "Infinity Ward", 2007),
@@ -29,12 +29,6 @@ if __name__ == '__main__':#ignore this it's here for testing reasons
     #this takes your schema and uses it to create a new table in your database.
     cursor.execute(schema_string)
     #this takes the values you put into your tables and adds their values into the database table
-    cursor.executemany(f"insert into {table_name} values (?,?,?)", table)#the number of question marks here should corrispond to the number of elements in your table
+    cursor.executemany(f"insert into {table_name} values (?,?,?)", table)#the number of question marks here should correspond to the number of elements in your table
     #saves your database
     connection.commit()
-
-def get_table_name():
-    return table_name
-
-
-
